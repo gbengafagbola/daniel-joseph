7bc5a6eabb21f38394e273bcbd7b7d7f90e5a849
holard <fagbolagbenga2015@gmail.com>
2022-03-21 19:46:52 +0100

    intro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/header/header";
+import Introduction from "./components/introduction/introduction";
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Introduction />
     </>
   );
 }
--- src/components/introduction/introduction.jsx
+++ src/components/introduction/introduction.jsx
@@ -1,12 +1,12 @@
 import './introduction.css';
 
-export default function Header() {
+export default function Introduction() {
     return (
-    <div className="main">
-        <div className="greetings"> HI 👋🏽, MY NAME IS <span>DANIEL JOSEPH</span> </div>
+    <div className="main-intro">
+        <div className="greetings"> HI 👋🏽, MY NAME IS <span> DANIEL JOSEPH</span> </div>
 
         <div className="description">
-        Product Design Lead at Yokunbo Studios(US), I was previously at eSoft Response(UK). Through curiosity, innovation and product strategy, I create enjoyable and intuitive user-centered design interfaces with an emphasis on visuals, interaction, design systems, and creative direction to aid accomplish business and user goals.
+            Product Design Lead at Yokunbo Studios(US), I was previously at eSoft Response(UK). Through curiosity, innovation and product strategy, I create enjoyable and intuitive user-centered design interfaces with an emphasis on visuals, interaction, design systems, and creative direction to aid accomplish business and user goals.
         </div>
     </div>
     )

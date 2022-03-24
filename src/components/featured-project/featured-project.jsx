9392134af9aaf6c56841ebdb10595fd8b3e63048
holard <fagbolagbenga2015@gmail.com>
2022-03-24 19:32:51 +0100

    other projects .

--- src/components/featured-project/featured-project.jsx
+++ src/components/featured-project/featured-project.jsx
@@ -1,9 +1,26 @@
 import './featured-project.css'
+import {ReactComponent as PlanetSvg} from '../../assets/svg/planet.svg';
+import {ReactComponent as SpecNotaSvg} from '../../assets/svg/Spec Nota.svg';
+
+import DjImage from '../../assets/images/dj.jpg';
 
 export default function FeaturedProject() {
     return (
     <div className="featured-container">
+
         <div className="featured-title">FEATURED PROJECTS</div>
+
+        <PlanetSvg className="planet"/>
+
+        <div className='projects'>
+            <div className='project-title'>Spec Nota
+               <div className='spec-nota'> <SpecNotaSvg  className='spec'/> </div>
+            </div>
+        </div>
+
+        <div className='project-preview'> 
+            {/* <div className='spec-nota'> <SpecNotaSvg  className='spec'/> </div> */}
+        </div>
     </div>
     )
 }

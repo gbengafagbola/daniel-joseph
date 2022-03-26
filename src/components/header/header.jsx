8d6538d461be11da97942a6f406693da97bfaf56
holard <fagbolagbenga2015@gmail.com>
2022-03-26 16:55:06 +0100

    dark mode home

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,7 @@
 import './header.css'
 import {ReactComponent as Logo} from '../../assets/svg/logo.svg';
-import {ReactComponent as Mode} from '../../assets/svg/mode.svg';
+import {ReactComponent as Light} from '../../assets/svg/light-mode.svg';
+import {ReactComponent as Dark} from '../../assets/svg/dark-mode.svg';
 
 export default function Header({darkMode, change}) {
   return (
@@ -17,11 +18,11 @@ export default function Header({darkMode, change}) {
           
           <nav>
               <ul className="nav-menu">
-                <li className="nav-item"> <a href="#">about me</a> </li>
-                <li className="nav-item"> <a href="#">work</a> </li>
-                <li className="nav-item"> <a href="#">resume</a> </li>
-                <li className="nav-item"> <a href="#">contact me</a> </li>
-                <li className="nav-item" onClick={change}> <Mode className="mode"/> </li>
+                <li className="nav-item"> about me </li>
+                <li className="nav-item"> work </li>
+                <li className="nav-item"> resume </li>
+                <li className="nav-item"> contact me </li>
+                <li className="nav-item" onClick={change}> { darkMode? <Light className="mode"/> : <Dark className='mode' /> } </li>
               </ul>
           </nav>
         </div>
